60784916c07ac5b2bf71bd1383463777a60e51d1
Johan Strandberg <johan@Johans-MacBook-Pro.local>
2016-11-27 13:59:35 +0100

    Added GLFW

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/go-gl/glfw/v3.2/glfw"
+)
 
 func main() {
-    fmt.Printf("kebab")
+    //fmt.Printf("kebab")
+    glfw.Init()
+    defer glfw.Terminate()
+    window, err := glfw.CreateWindow(640,480, "Bohica", nil, nil)
+    if err != nil {
+        panic(err)
+    }
+    window.MakeContextCurrent()
+    fmt.Println(glfw.VulkanSupported())
+        fmt.Println(glfw.GetVersion())
+
+    for !window.ShouldClose() {
+        window.SwapBuffers()
+        glfw.PollEvents()
+    }
 }
