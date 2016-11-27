f47e1612c9b5a9506814b8d23e14701f4d46df3b
Johan Strandberg <johan@Johans-MacBook-Pro.local>
2016-11-27 14:59:26 +0100

    GREEN SCREEN!

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "github.com/go-gl/gl/v3.2-compatibility/gl"
     "github.com/go-gl/glfw/v3.2/glfw"
 )
 
@@ -13,11 +14,18 @@ func main() {
     if err != nil {
         panic(err)
     }
+
+    if err := gl.Init(); err != nil {
+        panic(err)
+    }
+
     window.MakeContextCurrent()
     fmt.Println(glfw.VulkanSupported())
         fmt.Println(glfw.GetVersion())
 
     for !window.ShouldClose() {
+        gl.ClearColor(0.0, 1.0,0.0,0.0)
+        gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
         window.SwapBuffers()
         glfw.PollEvents()
     }
